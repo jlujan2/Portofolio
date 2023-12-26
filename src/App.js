dae9ae323e2abd494458b1643c7c5c68e7d5a917
unknown <jlujan6@gatech.edu>
2023-12-26 13:26:09 -0500

    added experience

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -17,7 +16,7 @@ function App() {
       <Banner />
       <About />
       <Experience />
-      <Skills />
+      {/* <Skills /> */}
       <Projects />
       <Contact />
       <Footer />
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -53,13 +53,30 @@ export const Experience = () => {
                 <div className="row" >
                     <div >
                         <Accordion defaultActiveKey="0" >
+
+                            <Accordion.Item className='accordion-item' eventKey="0">
+                            
+                            <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >BackEnd Engineer @ Chromatics Games</p></Accordion.Header>
+                    
+                            <Accordion.Body>
+                                <p>Developed backend high-quality, robust production code for DNA web application. Create Rest API following right practices such as versioning, accurate response calls, exception handling, pagination, and caching. Developed and maintained Microservices using Config Server, Circuit Breaker, API Gateway, etc. Consumed microservices using Axios from React application using React and Redux</p>
+                                <div>
+                                  <img src={javaIcon} alt="Image" style={{width:"3%", marginRight:"3px"}}/>
+                                  <img src={springBootIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  <img src={sqlIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  <img src={reactIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  
+                                </div>
+                            </Accordion.Body>
+                            
+                            </Accordion.Item>
                             
                             <Accordion.Item className='accordion-item' eventKey="0">
                             
-                            <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >Software Engineer @ Eurofins</p></Accordion.Header>
+                            <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >Application Engineer @ Eurofins</p></Accordion.Header>
                     
                             <Accordion.Body>
-                                <p>Developing back-end and database solutions in DNA Web App using Java, Sql in SpringBoot framework and ReactJs.</p>
+                                <p>Developed backend high-quality, robust production code for DNA web application. Create Rest API following right practices such as versioning, accurate response calls, exception handling, pagination, and caching. Developed and maintained Microservices using Config Server, Circuit Breaker, API Gateway, etc. Consumed microservices using Axios from React application using React and Redux</p>
                                 <div>
                                   <img src={javaIcon} alt="Image" style={{width:"3%", marginRight:"3px"}}/>
                                   <img src={springBootIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
@@ -91,6 +108,38 @@ export const Experience = () => {
                             <Accordion.Item className='accordion-item' eventKey="0">
                             
                             <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >Software Engineer @ Infotech INC</p></Accordion.Header>
+                    
+                            <Accordion.Body>
+                                <p>Developing back-end and database solutions in DNA Web App using Java, Sql in SpringBoot framework and ReactJs.</p>
+                                <div>
+                                  <img src={dotnetIcon} alt="Image" style={{width:"3%", marginRight:"3px"}}/>
+                                  <img src={sqlIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  <img src={javaScriptIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  
+                                </div>
+                            </Accordion.Body>
+                            
+                            </Accordion.Item>
+
+                            <Accordion.Item className='accordion-item' eventKey="0">
+
+                            <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >Software Engineer @ Florida Blue</p></Accordion.Header>
+                    
+                            <Accordion.Body>
+                                <p>Developing back-end and database solutions in DNA Web App using Java, Sql in SpringBoot framework and ReactJs.</p>
+                                <div>
+                                  <img src={dotnetIcon} alt="Image" style={{width:"3%", marginRight:"3px"}}/>
+                                  <img src={sqlIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  <img src={javaScriptIcon} alt="Image" style={{width:"3%", marginRight:"3px", marginLeft:"3px"}}/>
+                                  
+                                </div>
+                            </Accordion.Body>
+                            
+                            </Accordion.Item>
+
+                            <Accordion.Item className='accordion-item' eventKey="0">
+                              
+                            <Accordion.Header style={{ backgroundColor:"#4A2FBD"}}><p >Senior Software Engineer @ Mindtree</p></Accordion.Header>
                     
                             <Accordion.Body>
                                 <p>Developing back-end and database solutions in DNA Web App using Java, Sql in SpringBoot framework and ReactJs.</p>
